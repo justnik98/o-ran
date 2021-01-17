f4dcc9622c8953b54de8dae29574583cb8939bf3
Nikita Yankovskii <yannikita98@gmail.com>
2021-01-17 06:18:21 +0300

    added config reader for controller

--- main.cpp
+++ main.cpp
@@ -1,17 +1,49 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <stdexcept>
 #include <string>
 #include "controller/Controller.h"
+#include "reader/JSONReader.h"
 #include "writer/ConsoleWriter.h"
 #include "writer/HTTPWriter.h"
-#include "reader/JSONReader.h"
+
 
 using namespace std;
 
 int main() {
-    HTTPWriter h("127.0.0.1", "8080");
-    JSONReader r("warcraft-3-quotes.json");
-    ConsoleWriter w;
-    Controller c(w, r, "127.0.0.1");
-    c.run();
+    try {
+        map<string, string> params;
+        ifstream cfg("o-ran.cfg");
+        if (cfg.is_open()) {
+            string line;
+            while (getline(cfg, line)) {
+                line.erase(remove_if(line.begin(), line.end(), [](unsigned char x) { return std::isspace(x); }),
+                           line.end());
+                if (line[0] == '#' || line.empty()) continue;
+                auto delimiterPos = line.find('=');
+                auto name = line.substr(0, delimiterPos);
+                auto value = line.substr(delimiterPos + 1);
+                params.insert_or_assign(name, value);
+            }
+        } else {
+            cerr << "Couldn't open config file for reading. \n";
+        }
+        if (params["output"] == "http") {
+            HTTPWriter w(params["ip"], params["port"]);
+            JSONReader r(params["filename"]);
+            Controller c(w, r, params["redisip"], stoi(params["redisport"]));
+            c.run();
+        } else {
+            ConsoleWriter w;
+            JSONReader r("warcraft-3-quotes.json");
+            Controller c(w, r, params["redisip"], stoi(params["redisport"]));
+            c.run();
+        }
+    }
+    catch(std::exception e){
+        cerr << e.what();
+    }
     return 0;
 }
